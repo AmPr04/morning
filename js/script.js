82ce68fb32e04a1c5a154c71f73ea34a1a41443c
AmPr04 <pradoamanda65@gmail.com>
2025-06-21 00:08:49 -0400

    night

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const day = document.querySelector('.afternoon');
 // You can customize the time ranges as needed
 if (hour >= 6 && hour < 12) {
   sky.classList.add('visible');
-} else if (hour >= 19| hour < 5) {
+} else if (hour >= 19 && hour < 5) {
   night.classList.add('visible');
 } else (hour >= 12 && hour < 19); {
   day.classList.add('visible');
