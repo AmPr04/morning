381681c0c514c8a3219f9c65c0bf0aac42cb52dd
AmPr04 <pradoamanda65@gmail.com>
2025-06-21 00:10:35 -0400

    night

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ if (hour >= 6 && hour < 12) {
   sky.classList.add('visible');
 } else if (hour >= 19 && hour < 5) {
   night.classList.add('visible');
-} else (hour >= 12 && hour < 19); {
+} else (hour >= 12 && hour < 18); {
   day.classList.add('visible');
 }
 
